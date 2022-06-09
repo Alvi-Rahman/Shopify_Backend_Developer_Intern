700537f8a117789fe6ffd0b5ef3c4bff5144d6ef
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-09 22:20:34 +0100

    feat: InventotyType model

--- shopify/models.py
+++ shopify/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class InventoryType(models.Model):
+    type_name = models.CharField(max_length=255)
+    type_description = models.TextField(blank=True, null=True)
+    active_status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.type_name
+
