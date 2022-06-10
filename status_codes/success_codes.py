0763b538f0376ab6a9c787d23233ca68e832c590
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-10 11:28:18 +0100

    fix: add suceess codes

--- status_codes/success_codes.py
+++ status_codes/success_codes.py
@@ -1,8 +1,16 @@
 from utils.response_base import CodeObject
 
+INVENTORY_TYPE_CREATE_SUCCESS = CodeObject(
+    http_status=200,
+    state_code="ITS2001",
+    state_message={
+        "en": "Inventory Type Create Success"
+    }
+)
+
 INVENTORY_CREATE_SUCCESS = CodeObject(
     http_status=200,
-    state_code="ICS2001",
+    state_code="ICS2002",
     state_message={
         "en": "Inventory Create Success"
     }
