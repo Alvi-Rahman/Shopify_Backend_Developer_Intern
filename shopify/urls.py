24c710493c85573c1110413d140c469d1ec890e7
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-10 15:37:55 +0100

    feat: list api url

--- shopify/urls.py
+++ shopify/urls.py
@@ -2,8 +2,6 @@ from django.urls import path
 from .views import (InventoryViewSet, InventoryTypeViewSet)
 
 inventory_type_urlpatterns = [
-    path('inventory/create/', InventoryViewSet.as_view({"post": "create"}),
-         name="inventory_create"),
     path('inventory-type/create/', InventoryTypeViewSet.as_view({"post": "create"}),
          name="inventory_type_create"),
     path('inventory-type/all/', InventoryTypeViewSet.as_view({"get": "list"}),
@@ -16,3 +14,13 @@ inventory_type_urlpatterns = [
         }
     )),
 ]
+
+inventory_urlpatterns = [
+    path('inventory/create/', InventoryViewSet.as_view({"post": "create"}),
+         name="inventory_create"),
+    path('inventory/all/', InventoryTypeViewSet.as_view({"get": "list"}),
+         name="inventory_type_list"),
+    
+]
+
+urlpatterns = inventory_type_urlpatterns + inventory_urlpatterns
