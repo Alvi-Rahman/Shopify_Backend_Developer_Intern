5a7ea7dc5ce7b0973aba4165a85421c13794a883
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-10 12:03:42 +0100

    fix: make times nullable

--- shopify/models.py
+++ shopify/models.py
@@ -8,8 +8,8 @@ class InventoryType(models.Model):
     type_name = models.CharField(max_length=255)
     type_description = models.TextField(blank=True, null=True)
     active_status = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self):
         return self.type_name
@@ -21,8 +21,8 @@ class Inventory(models.Model):
     stock_count = models.PositiveIntegerField(default=0)
     inventory_type = models.ForeignKey(InventoryType, on_delete=models.CASCADE)
     active_status = models.BooleanField(default=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     def __str__(self):
         return self.title
