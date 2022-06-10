d2123d2f5ac0f83689c60c6e145d6ccc46fa3adc
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-10 12:01:15 +0100

    add: created_at and updated_at for InventoryType model

--- shopify/models.py
+++ shopify/models.py
@@ -8,6 +8,8 @@ class InventoryType(models.Model):
     type_name = models.CharField(max_length=255)
     type_description = models.TextField(blank=True, null=True)
     active_status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.type_name
