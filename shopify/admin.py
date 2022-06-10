0560dfea4b882e0638982836a84aaa966f470bab
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-10 11:24:25 +0100

    feat: admin registration

--- shopify/admin.py
+++ shopify/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import *
-# Register your models here.
+
+
+class ErrorLogAdmin(admin.ModelAdmin):
+    list_display = ["log_type",
+                    "created_at",
+                    "updated_at",
+                    ]
+    search_fields = ["log_type",]
+
+    list_filter = ['log_type',]
+
 
 admin.site.register(InventoryType)
 admin.site.register(Inventory)
+admin.site.register(ErrorLog, ErrorLogAdmin)
+
+
