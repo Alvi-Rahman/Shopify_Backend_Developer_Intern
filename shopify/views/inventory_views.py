5f37760661d3560075f34ac13e9e75b3ed7bc890
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-10 15:11:30 +0100

    fix: override get_object for bug fix

--- shopify/views/inventory_views.py
+++ shopify/views/inventory_views.py
@@ -1,7 +1,7 @@
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
-from shopify.models import ErrorLog
+from shopify.models import ErrorLog, Inventory
 from shopify.serializers import (InventoryCreateSerializer,
                                  InventoryGetSerializer,)
 from status_codes import error_codes, success_codes
@@ -11,6 +11,33 @@ from utils.response_utils import ResponseWrapper
 class InventoryViewSet(ModelViewSet):
     response_wrapper = ResponseWrapper()
     language = "en"
+
+    queryset = Inventory.objects.all()
+
+    def get_object(self):
+        """
+        Returns the object the view is displaying.
+
+        You may want to override this if you need to provide non-standard
+        queryset lookups.  Eg if objects are referenced using multiple
+        keyword arguments in the url conf.
+        """
+        queryset = self.filter_queryset(self.get_queryset())
+
+        # Perform the lookup filtering.
+        lookup_url_kwarg = self.lookup_url_kwarg or self.lookup_field
+
+        assert lookup_url_kwarg in self.kwargs, (
+                'Expected view %s to be called with a URL keyword argument '
+                'named "%s". Fix your URL conf, or set the `.lookup_field` '
+                'attribute on the view correctly.' %
+                (self.__class__.__name__, lookup_url_kwarg)
+        )
+
+        filter_kwargs = {self.lookup_field: self.kwargs[lookup_url_kwarg]}
+        obj = queryset.filter(**filter_kwargs).first()
+
+        return obj
 
     def get_serializer_class(self):
         if self.action == "create":
