2d3283a9e3514f33c2c4f739c8c052a3f76071e0
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-10 10:58:50 +0100

    feat: inventory type create in url

--- shopify/urls.py
+++ shopify/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from .views import (InventoryViewSet, InventoryTypeViewSet)
 
 urlpatterns = [
-    
+    path('inventory/create/', InventoryViewSet.as_view(
+        {
+            "post": "create"
+        }
+    )),
 ]
