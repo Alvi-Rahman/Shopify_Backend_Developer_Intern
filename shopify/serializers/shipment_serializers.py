f106fedea9acb4f25e71a11d6ad64c1493f0f636
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-12 17:34:27 +0100

    feat: ShipmentContainerGetSerializer

--- shopify/serializers/shipment_serializers.py
+++ shopify/serializers/shipment_serializers.py
@@ -1,20 +1,18 @@
 from rest_framework.serializers import ModelSerializer
 
 from shopify.models import Shipment, ShipmentContainer
-from .inventory_serializer import InventoryCreateSerializer
+from .inventory_serializer import InventoryGetSerializer
 
 
 class ShipmentContainerCreateSerializer(ModelSerializer):
-    added_products = InventoryCreateSerializer()
-
     class Meta:
         model = ShipmentContainer
         fields = "__all__"
 
 
-class ShipmentCreateSerializer(ModelSerializer):
-    inventor_per_shipment = ShipmentContainerCreateSerializer(many=True)
+class ShipmentContainerGetSerializer(ModelSerializer):
+    added_products = InventoryGetSerializer
 
     class Meta:
-        model = Shipment
+        model = ShipmentContainer
         fields = "__all__"
