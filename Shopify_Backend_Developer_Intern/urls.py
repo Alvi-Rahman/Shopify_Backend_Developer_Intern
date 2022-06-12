3cb43ce37b28ee22cca5c55ea4f7d2e6171e1215
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-12 18:30:02 +0100

    fix: update url for docs

--- Shopify_Backend_Developer_Intern/urls.py
+++ Shopify_Backend_Developer_Intern/urls.py
@@ -35,10 +35,11 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
     path('shopify/', include(shopify_app_url_patterns)),
     # path('swagger/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('apis/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
