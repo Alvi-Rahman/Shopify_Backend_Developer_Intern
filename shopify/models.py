9f5a3a9da1b91c7e8a1357cff2e936bea5a220d9
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-12 16:46:55 +0100

    fix: place shipment logs at the last

--- shopify/models.py
+++ shopify/models.py
@@ -3,9 +3,6 @@ import uuid
 from django.db import models
 
 
-# Create your models here.
-
-
 class InventoryType(models.Model):
     type_name = models.CharField(max_length=255)
     type_description = models.TextField(blank=True, null=True)
@@ -34,30 +31,6 @@ class Inventory(models.Model):
         verbose_name_plural = "Inventories"
 
 
-class ErrorLog(models.Model):
-    LOG_TYPE_CHOICES = [("INVENTORY_TYPE", "Inventory Type"),
-                        ("INVENTORY", "Inventory"), ]
-    STATE_CHOICES = [("CREATE", "CREATE"),
-                     ("LIST", "LIST"),
-                     ("GET", "GET"),
-                     ("UPDATE", "UPDATE"),
-                     ("DELETE", "DELETE"), ]
-    log_type = models.CharField(
-        max_length=25, choices=LOG_TYPE_CHOICES, default="INVENTORY")
-    request_data = models.JSONField(blank=True, null=True)
-    response_data = models.JSONField(blank=True, null=True)
-    misc_data = models.TextField(blank=True, null=True)
-
-    state = models.CharField(
-        max_length=25, choices=STATE_CHOICES, default="CREATE")
-
-    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
-
-    def __str__(self):
-        return self.log_type
-
-
 class ShipmentContainer(models.Model):
     id = models.AutoField(primary_key=True)
     cart_code = models.UUIDField(default=uuid.uuid4)
@@ -80,3 +53,27 @@ class Shipment(models.Model):
 
     def __str__(self):
         return self.shipment_id
+
+
+class ErrorLog(models.Model):
+    LOG_TYPE_CHOICES = [("INVENTORY_TYPE", "Inventory Type"),
+                        ("INVENTORY", "Inventory"), ]
+    STATE_CHOICES = [("CREATE", "CREATE"),
+                     ("LIST", "LIST"),
+                     ("GET", "GET"),
+                     ("UPDATE", "UPDATE"),
+                     ("DELETE", "DELETE"), ]
+    log_type = models.CharField(
+        max_length=25, choices=LOG_TYPE_CHOICES, default="INVENTORY")
+    request_data = models.JSONField(blank=True, null=True)
+    response_data = models.JSONField(blank=True, null=True)
+    misc_data = models.TextField(blank=True, null=True)
+
+    state = models.CharField(
+        max_length=25, choices=STATE_CHOICES, default="CREATE")
+
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
+
+    def __str__(self):
+        return self.log_type
