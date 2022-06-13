959af609dd4c0241c5c884f28796e1ea82a71635
Alvi Rahman <takrim.rahman.albi@g.bracu.ac.bd>
2022-06-13 20:28:36 +0100

    fix: rename serializer field name

--- shopify/serializers/shipment_serializers.py
+++ shopify/serializers/shipment_serializers.py
@@ -19,7 +19,7 @@ class ShipmentContainerGetSerializer(ModelSerializer):
 
 
 class ShipmentCreateSerializer(ModelSerializer):
-    inventor_per_shipment = ShipmentContainerCreateSerializer(many=True)
+    inventory_per_shipment = ShipmentContainerCreateSerializer(many=True)
 
     class Meta:
         model = Shipment
